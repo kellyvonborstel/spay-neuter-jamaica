83ed1b6ef640979e50045344879865061a822949
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-08-21 18:24:18 -0700

    Move sidebar below main content on small screens

--- public_html/src/main.js
+++ public_html/src/main.js
@@ -2,6 +2,7 @@ $(function() {
 
   addImageHandlers();
 
+
   $('#slider').responsiveSlides({
     auto: true,
     pager: false,
@@ -10,6 +11,21 @@ $(function() {
     maxwidth: 800,
     namespace: 'large-btns'
   });
+
+
+  // change order of sidebar items when window is resized
+  $(window).resize(function () {
+    var windowWidth = $(window).width();
+
+    if (windowWidth >= 870) {
+      $(".sidebar-content").remove().insertBefore($(".main-content"));
+      addImageHandlers();
+    }
+    else {
+      $(".main-content").remove().insertBefore($(".sidebar-content"));
+    }
+  }).trigger("resize");
+
 
   // added the click handler to prevent images from disappearing on ios
   function imageHandler(className, enterImgPath, leaveImgPath) {
@@ -26,9 +42,11 @@ $(function() {
     });
   }
 
+
   function addImageHandlers() {
     // class names with '-home' included because path is different for images on index.html
     imageHandler('.js-linkedin-home', 'images/linkedin-dark.svg', 'images/linkedin.svg');
     imageHandler('.js-linkedin', '../images/linkedin-dark.svg', '../images/linkedin.svg');
   }
+
 });
